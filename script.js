9f6f92b9438fad1404dab7dff9b511fb4023d7e2
Diane Saia <pontesaia@gmail.com>
2021-02-21 12:01:39 -0500

    js math

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 
-var p = document.getElementById("principal").value;
-var r = document.getElementById("rate").value;
-var d = new Date();
-var year = d.getFullYear();
-var y = document.getElementById("year").value;
-var c;
-var amount = p * r;
-var c = year + y;
+
 
 function compute() {
-    var results =
-        "If you deposit" + p + "<br/>"
-        + "at an interest rate of" + r + "<br/>"
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var d = new Date();
+    var year = d.getFullYear();
+    var y = document.getElementById("years").value;
+    var c;
+    var amount = (p * r/100) * y;
+    var c = parseInt(year) + parseInt(y);
+
+    document.getElementById('result').innerHTML = 
+        "If you deposit " + p + "<br/>"
+        + "at an interest rate of " + r + "% <br/>"
         + "You will recieve an amount of " + amount + "<br/>"
-        + "in the year" + c + "<br/>";
-    console.log(results);
-    return results;
+        + "in the year " + c + "<br/>"; 
+
 
 }
 
-document.getElementById('result').innerHTML = results;  
