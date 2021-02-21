c29110d5dab20fd9aeec0b9e806daf8234b6a700
Diane Saia <pontesaia@gmail.com>
2021-02-21 10:41:49 -0500

    styling

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+var p = document.getElementById("principal").value;
+var r = document.getElementById("rate").value;
+var d = new Date();
+var year = d.getFullYear();
+var y = document.getElementById("year").value;
+var c;
+var amount = p * r;
+var c = year + y;
+
+function compute() {
+    var results =
+        "If you deposit" + p + "<br/>"
+        + "at an interest rate of" + r + "<br/>"
+        + "You will recieve an amount of " + amount + "<br/>"
+        + "in the year" + c + "<br/>";
+    console.log(results);
+    return results;
+
 }
-        
+
+document.getElementById('result').innerHTML = results;  
